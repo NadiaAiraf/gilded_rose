4fbc13b81e1f1bebe6a1d5e852ed4afab9de1bfe
NadiaAiraf <aidanfaria@gmail.com>
2018-11-11 15:26:52 +0000

    simple changes to make it shorter

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -9,22 +9,18 @@ class GildedRose
       if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
         if item.quality > 0
           if item.name != "Sulfuras, Hand of Ragnaros"
-            item.quality = item.quality - 1
+            item.quality -= 1
           end
         end
       else
         if item.quality < 50
-          item.quality = item.quality + 1
+          item.quality += 1
           if item.name == "Backstage passes to a TAFKAL80ETC concert"
             if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
+              item.quality += 1
             end
             if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
+              item.quality += 1
             end
           end
         end
@@ -45,7 +41,7 @@ class GildedRose
           end
         else
           if item.quality < 50
-            item.quality = item.quality + 1
+            item.quality += 1
           end
         end
       end
