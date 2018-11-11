fd7f16b0f133309a93a708515809e3c043e9dc2e
NadiaAiraf <aidanfaria@gmail.com>
2018-11-11 15:57:53 +0000

    added test for reduce_value method

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -1,3 +1,17 @@
 describe Item do
-  describe '# reduce_value'
+  subject { described_class.new('apples', 1, 5) }
+
+  describe '# reduce_value' do
+    it 'reduces the value of apples by 1' do
+      subject.reduce_value
+      expect(subject.value).to eq 4
+    end
+
+    it 'reduces the value of apples by 2 once past sell date' do
+      subject.reduce_value
+      subject.reduce_value
+      subject.reduce_value
+      expect(subject.value).to eq 2
+    end
+  end
 end
