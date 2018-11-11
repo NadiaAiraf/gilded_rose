4e445f7487eeadc5874c08c32c4f0c57a0d1994c
NadiaAiraf <aidanfaria@gmail.com>
2018-11-11 18:41:48 +0000

    updated gilded rose to now work from the classes created

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -1,58 +1,12 @@
 class GildedRose
-
-  NONPERISHABLES = ["Aged Brie",
-                    "Backstage passes to a TAFKAL80ETC concert",
-                    "Sulfuras, Hand of Ragnaros"]
-
-  NOSELLBY = ["Sulfuras, Hand of Ragnaros"]
-
   def initialize(items)
     @items = items
   end
 
   def update_quality()
     @items.each do |item|
-      if !(NONPERISHABLES.include?(item.name))
-        if item.quality > 0
-          item.quality -= 1
-        end
-      else
-        if item.quality < 50
-          item.quality += 1
-          if item.name == "Backstage passes to a TAFKAL80ETC concert"
-            if item.sell_in < 11
-              item.quality += 1
-            end
-            if item.sell_in < 6
-              item.quality += 1
-            end
-          end
-        end
-      end
-
-      #changes the sell by date
-      unless NOSELLBY.include?(item.name)
-        item.sell_in = item.sell_in - 1
-      end
-
-      if item.sell_in < 0
-        if item.name != "Aged Brie"
-          if item.name != "Backstage passes to a TAFKAL80ETC concert"
-            if item.quality > 0
-              if item.name != "Sulfuras, Hand of Ragnaros"
-                item.quality = item.quality - 1
-              end
-            end
-          else
-            item.quality = item.quality - item.quality
-          end
-        else
-          if item.quality < 50
-            item.quality += 1
-          end
-        end
-      end
-
+      item.update_quality
+      item.update_sell_in
     end
   end
 end
